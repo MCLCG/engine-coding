f6ca2b74ba95ac552e5069364ba7172bd448b760
李常国 <1282128587@qq.com>
2024-03-14 16:29:34 +0800

    fix(name): b c fixpackages e

--- packages/gg-lint-config/__tests__/gg-lint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const ggLintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(ggLintConfig(), 'Hello from ggLintConfig');
-console.info('ggLintConfig tests passed');
